99534544555d539c9e80bb170d8d3ec8a59f50aa
FiveMovesAhead <ying@tig.foundation>
2025-08-01 13:05:45 +0100

    WIP

--- vector_search_evaluator/run.sh
+++ vector_search_evaluator/run.sh
@@ -21,9 +21,9 @@ sed "s/\${ALGORITHM}/$ALGORITHM/g" src/main.rs.template > src/main.rs
 # Check if src/$ALGORITHM.rs exists
 if [ -f "src/$ALGORITHM.rs" ]; then
     echo "Using local algorithm in src/$ALGORITHM.rs"
-    cargo run --release --features local -- "$TEST_INSTANCES_FOLDER"
+    ALGORITHM=$ALGORITHM cargo run --release --features local -- "$TEST_INSTANCES_FOLDER"
 else
     echo "Using algorithm from github:"
     echo "https://github.com/tig-foundation/tig-monorepo/blob/vector_search/$ALGORITHM/tig-algorithms/src/vector_search/$ALGORITHM/benchmarker_outbound.cu" 
-    cargo run --release -- "$TEST_INSTANCES_FOLDER"
+    ALGORITHM=$ALGORITHM cargo run --release -- "$TEST_INSTANCES_FOLDER"
 fi
