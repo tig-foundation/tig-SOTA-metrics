8cbde746008a6d07c52ea3abfc32d09deed6af46
FiveMovesAhead <ying@tig.foundation>
2025-08-01 12:10:00 +0100

    WIP

--- vehicle_routing_evaluator/data/download_HG_instances.py
+++ vehicle_routing_evaluator/data/download_HG_instances.py
@@ -2,15 +2,33 @@
 # Download Homberger and Gehring .txt files and matching .sol files
 ### 
 
+import concurrent.futures
+import time
 import itertools
 import requests
 import os
 
+if not os.path.exists('GH'):
+    os.makedirs('GH')
+
+MAX_WORKERS = 8
 BASE_URL = "http://vrp.galgos.inf.puc-rio.br/media/com_vrp/instances/HG/"
 
-print('Downloading HG instances...')
-os.makedirs('HG', exist_ok=True)
+def download_instance(download_url, save_path):
+    for i in range(3):
+        try:
+            print('Downloading ' + download_url)
+            resp = requests.get(download_url)
+            resp.raise_for_status()
+            with open(save_path, 'w') as f:
+                f.write(resp.text)
+            break
+        except Exception as e:
+            print(f"Error downloading {download_url}: {e}")
+            print(f"Retrying {download_url}: {i+1} of 3")
+            time.sleep(i * 2)
 
+tasks = []
 for combo in itertools.product(
     ['C1', 'C2', 'R1', 'R2', 'RC1', 'RC2'],
     [2, 4, 6, 8, 10],
@@ -19,10 +37,12 @@ for combo in itertools.product(
 ):
     name = f"{combo[0]}_{combo[1]}_{combo[2]}{combo[3]}"
     url = f"{BASE_URL}{name}"
-    print('Downloading ' + url)
-    resp = requests.get(url)
-    resp.raise_for_status()
-    with open(os.path.join('GH', name), 'w') as f:
-        f.write(resp.text)
+    save_path = os.path.join('GH', name)
+    tasks.append((url, save_path))
 
-print("Done")
+with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+    futures = [executor.submit(download_instance, url, save_path) for url, save_path in tasks]
+    for future in concurrent.futures.as_completed(futures):
+        pass
+
+print("Done.")
